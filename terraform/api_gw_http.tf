78ef6f2524592851d8f4049130a4c27ed8faffe1
flarkmanagan <markf121@hotmail.com>
2025-01-09 11:24:40 +0000

    added tf files

--- terraform/api_gw_http.tf
+++ terraform/api_gw_http.tf
@@ -9,31 +9,20 @@ resource "aws_apigatewayv2_api" "lambda_http_api" {
 
 resource "aws_apigatewayv2_route" "api_route" {
   api_id = aws_apigatewayv2_api.lambda_http_api.id
-  route_key = "GET /VisitorCounterUpdater"
+  route_key = "GET /${var.lambda_name}" # change string to "/VisitorCounterUpdater" if var ref doesn't work
   target = "integrations/${aws_apigatewayv2_integration.api_lambda_integration.id}"
 }
 
 resource "aws_apigatewayv2_integration" "api_lambda_integration" {
-   api_id = aws_apigatewayv2_api.lambda_http_api.id
-   integration_type = "AWS_PROXY"
-   connection_type = "INTERNET"
-   integration_method = "POST"
-   integration_uri = aws_lambda_function.lambda_counter.invoke_arn
+  api_id = aws_apigatewayv2_api.lambda_http_api.id
+  integration_type = "AWS_PROXY"
+  connection_type = "INTERNET"
+  integration_method = "POST"
+  integration_uri = aws_lambda_function.lambda_counter.invoke_arn
 }
 
-/*
-resource "aws_apigatewayv2_integration_response" "api_lambda_integration_response" {
-  api_id                   = aws_apigatewayv2_api.lambda_http_api.id
-  integration_id           = aws_apigatewayv2_integration.api_lambda_integration.id
-  integration_response_key = "/200/"
-
-  depends_on = [ aws_apigatewayv2_api.lambda_http_api ]
-}
-*/
-
-resource "aws_apigatewayv2_stage" "name" {
+resource "aws_apigatewayv2_stage" "api_stage" {
   name = "$default"
-  api_id                   = aws_apigatewayv2_api.lambda_http_api.id
+  api_id = aws_apigatewayv2_api.lambda_http_api.id
   auto_deploy = "true"
-
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,8 +2,7 @@ output "cloudfront_url" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
-/*
 output "api_invoke_url" {
-  value = aws_api_gateway_stage.api_stage_prod.invoke_url
+  value = aws_apigatewayv2_stage.api_stage.invoke_url
 }
-*/
+

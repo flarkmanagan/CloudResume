d0ce0084556c455bb29eb00ae79fb15b8a80fe64
flarkmanagan <markf121@hotmail.com>
2025-01-21 16:18:07 +0000

    Cleaned up old files after directory restructure

--- scripts/LambdaCaller.js
+++
@@ -1,8 +0,0 @@
-fetch('https://f9fm79pz96.execute-api.eu-west-1.amazonaws.com/prod/update-count')
-.then(data => {
-return data.json();
-})
-.then(post => {
-console.log(post.visitorCount);
-document.getElementById("hitCounter").innerHTML = post.visitorCount;
-});

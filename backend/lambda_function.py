261d828cb30b3c291104b8527155059bf363b434
flarkmanagan <markf121@hotmail.com>
2025-01-24 14:20:46 +0000

    testing backend workflow

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -9,7 +9,6 @@ table = dynamodb.Table('SiteVisits')
 def lambda_handler(event, context):
     try:
         response = table.get_item(Key={'id': 'visits'})
-        #Added comment for CICD testing purposes 
 
         if 'Item' not in response:
             table.put_item(Item={'id': 'visits', 'visitorCount': 0})

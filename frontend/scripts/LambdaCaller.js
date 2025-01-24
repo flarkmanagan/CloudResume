3ca6a38267ef0d982068b16aabf61b7318f6b5a8
flarkmanagan <markf121@hotmail.com>
2025-01-24 14:28:39 +0000

    testing frontend workflow

--- frontend/scripts/LambdaCaller.js
+++ frontend/scripts/LambdaCaller.js
@@ -1,4 +1,4 @@
-fetch('https://uklka6v4r1.execute-api.eu-west-1.amazonaws.com/VisitorCounterUpdater1')
+fetch('https://uklka6v4r1.execute-api.eu-west-1.amazonaws.com/VisitorCounterUpdater')
 .then(data => {
 return data.json();
 })

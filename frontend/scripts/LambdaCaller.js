e7ac9561b9a3d314d880ebc7ed72100b883aa302
flarkmanagan <markf121@hotmail.com>
2025-01-25 10:03:28 +0000

    Updated js to use url placeholder. api gw url added at build time

--- frontend/scripts/LambdaCaller.js
+++ frontend/scripts/LambdaCaller.js
@@ -1,6 +1,6 @@
 const apiGatewayURL = "<API_GATEWAY_URL>"
 
-fetch('https://uklka6v4r1.execute-api.eu-west-1.amazonaws.com/VisitorCounterUpdater')
+fetch(apiGatewayURL) /*'https://uklka6v4r1.execute-api.eu-west-1.amazonaws.com/VisitorCounterUpdater' */
 .then(data => {
 return data.json();
 })

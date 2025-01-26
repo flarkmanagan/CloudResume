ca5c4740b90b4f727a2e2f159558c2cd5875d2aa
flarkmanagan <markf121@hotmail.com>
2025-01-26 11:40:46 +0000

    Removed old commented out code for uploading objects to bucket

--- terraform/s3_bucket.tf
+++ terraform/s3_bucket.tf
@@ -19,19 +19,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "web_bucket_sse_co
    bucket_key_enabled = true 
   }
 }
-
-/*
-resource "aws_s3_object" "html" {
-  bucket = aws_s3_bucket.web_bucket.bucket
-  key = "index.html"
-  source = "${path.module}/../index.html"
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "js" {
-  bucket = aws_s3_bucket.web_bucket.bucket
-  key = "scripts/LambdaCaller.js"
-  source = "${path.module}/../scripts/LambdaCaller.js"
-  content_type = "application/javascript"
-}
-*/

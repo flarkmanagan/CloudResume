5b6be6bfa27eddadabd35270b9378c182c1e18ac
flarkmanagan <markf121@hotmail.com>
2025-01-28 17:46:23 +0000

    Added cypress test

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,20 @@
+describe('API Test', () => {
+  const invokeUrl = '<API_URL>';
+
+  it('Return visit count and 200 response code', () => {
+    cy.request({
+      method: 'GET',
+      url: invokeUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      console.log(response.headers['content-type']);
+      expect(response.status).to.eq(200);
+      const responseBody = typeof response.body === 'string' ? JSON.parse(response.body) : response.body;
+      expect(responseBody).to.have.property('visitorCount');
+      expect(responseBody.visitorCount).to.be.a('number');
+      expect(responseBody.visitorCount).to.be.greaterThan(0);
+    });
+  });
+});
